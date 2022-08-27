a7f6efa9504719de310e3db3db60faad774f1b80
Elango Sengottaiyan <elango.sengottaiyan@outlook.com>
2022-08-27 13:22:51 +0530

    Add basic file to implement the concepts

---
+++ Learning_ObjectOriented/Encapsulation/Encapsulation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_ObjectOriented.Encapsulation
+{
+    internal class Encapsulation
+    {
+    }
+}
